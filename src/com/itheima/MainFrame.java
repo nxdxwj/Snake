024d899d62b8c640aaccffe68efc5b6d7f45d98c
nxdxwj <202383910029@nuist.edu.cn>
2023-10-16 15:58:46 +0800

    Update

---
+++ src/com/itheima/MainFrame.java
@@ -0,0 +1,28 @@
+package com.itheima;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() throws HeadlessException {
+        //初始化窗口参数
+        initFrame();
+    }
+
+    private void initFrame() {
+        //设置窗口宽高
+        setSize(610,640);
+        //设置窗口位置
+        setLocation(400,400);
+        //设置关闭按钮
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //设置窗口大小不可变
+        setResizable(false);
+    }
+
+    //主程序
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+    }
+}
